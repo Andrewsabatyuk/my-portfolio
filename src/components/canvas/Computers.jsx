9f7ca8db6a5dd8632c4a3956e4851197eeaa4f42
Andrew <andrewsabatyuk17@gmail.com>
2023-05-03 20:50:03 +0300

    create Loader component

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -14,8 +14,8 @@ const Computers = () => {
       <primitive
         object={computer.scene}
         scale={0.75}
-        position={[0, -3.25, -1.5]}
-        rotation={[-0.1, -0.2, -0.1]}
+        position={[0.8, -3.25, -1.5]}
+        rotation={[0, -0.08, -0.05]}
       />
     </mesh>
   )
@@ -36,7 +36,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers isMobile={isMobile} />
+        <Computers  />
       </Suspense>
 
       <Preload all />
