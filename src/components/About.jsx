63570267d4253fab1646bfab2e2ac000b57f1fd4
Andrew <andrewsabatyuk17@gmail.com>
2023-05-03 21:29:02 +0300

    add styling and content to About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Tilt } from 'react-tilt'
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import { services } from '../constants'
+import { fadeIn, textVariant } from '../utils/motion'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p variants={fadeIn('', '', 0.2, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>I'm self motivated and hard work person, love to study and learn new features. Always open for new experiance.
+        I want to evolve myself as a frontend developer, that is my main goal.</motion.p>
+    </>
   )
 }
 
