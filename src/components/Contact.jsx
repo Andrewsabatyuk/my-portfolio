581f9aeba4aa2329c79fa213970eb8ea73d914b2
Andrew <andrewsabatyuk17@gmail.com>
2023-06-21 21:54:01 +0300

    add logic to Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,8 +16,51 @@ const Contact = () => {
     message: "",
   });
 
-  const handleChange = e => { }
-  const handleSubmit = e => { }
+  const handleChange = e => {
+    const { target } = e;
+    const { name, value } = target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  }
+  const handleSubmit = e => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: "Andrew Sabatyuk",
+          from_email: form.email,
+          to_email: "myfavorite@email.com.ua",
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
+
+          alert("Ahh, something went wrong. Please try again.");
+        }
+      );
+  }
 
   return (
     <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
